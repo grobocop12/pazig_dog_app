8e83f04ede4a8d827d029ed1f524b25b6c832089
grobocop <chasnik97876@gmail.com>
2018-12-18 23:29:39 +0100

    navigation

--- app/src/main/java/com/grobocop/dogapp/MainActivity.kt
+++ app/src/main/java/com/grobocop/dogapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.grobocop.dogapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val random_dog_button = findViewById<Button>(R.id.random_dog_button)
+        random_dog_button.setOnClickListener{
+            randomDogButtonClicked()
+        }
+
+        val dogs_list_button = findViewById<Button>(R.id.dogs_list_button)
+        dogs_list_button.setOnClickListener{
+            dogsListButtonClicked()
+        }
+    }
+
+    fun randomDogButtonClicked() {
+        val intent =  Intent(this, RandomDogActivity::class.java).apply { }
+        startActivity(intent)
+    }
+
+    fun dogsListButtonClicked(){
+        val intent =  Intent(this, DogsListActivity::class.java).apply { }
+        startActivity(intent)
     }
 }
