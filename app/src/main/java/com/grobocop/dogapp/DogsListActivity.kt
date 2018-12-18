c910452a0b2c53c107ba660eed6518698f921e65
grobocop <chasnik97876@gmail.com>
2018-12-18 22:34:32 +0100

    main activity layout

---
+++ app/src/main/java/com/grobocop/dogapp/DogsListActivity.kt
@@ -0,0 +1,12 @@
+package com.grobocop.dogapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DogsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dogs_list)
+    }
+}
